eca7d029e802376ac6470997b1bb35e747cf8032
Lukas Lewandowski <llewandowski1990@gmail.com>
2021-10-21 21:17:09 +0200

    add function for scraping data from html

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # Simple web scraper in nodejs.
 
 ## Installation
+
 ### Install node:
-```brew install nvm```
-```nvm install --lts```
+
+`brew install nvm`
+`nvm install --lts`
+
 ### Install nodemon:
-```npm install -g nodemon```
+
+`npm install -g nodemon`
 
 ### Install dependencies:
-```npm install```
+
+`npm install`
 
 ### Start the Express server:
-```npm start```
 
+`npm start`
 
+#### Kill server running in background:
 
+`lsof -i :8000`
+`kill -9 <PROCESS_PID>`
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express');
 const express = require('express');
 
+// TODO: add option to pass this from CLI
 const PORT = 8000;
+const URL = 'https://www.theguardian.com/international';
 
 const app = express();
 
-app.listen(PORT, () => console.log('server running on port ${PORT}'));
+axios(URL)
+	.then((response) => {
+		const responseData = response.data;
+		const loadedData = cheerio.load(responseData);
+		const articles = [];
+
+		// console.log('---------WHOLE HTML: ' + responseData);
+		/** fc-item__title is a class name of div element on The Guardian website
+		 *  which contains elements with data that we want */
+		loadedData('.fc-item__title', responseData).each(function () {
+			//<-- cannot be a function expression
+			const title = loadedData(this).text().trim();
+			const articleUrl = loadedData(this).find('a').attr('href');
+			/** push title and article to empty array and structure it as object */
+			articles.push({
+				title,
+				articleUrl,
+			});
+		});
+		console.log(articles);
+		// TODO: save result to file
+	})
+	.catch((error) => console.error(error));
+
+app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
